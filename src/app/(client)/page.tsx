32d23f70ff8c5c590e4c431e014334263c1f1d5d
Shenon69 <ask.tikki69@gmail.com>
2023-12-02 02:51:25 +0530

    fix: Common components import

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/green-supermarket-common-ui";
+
 export default function Home() {
   return (
     <>
       <h1>Green SuperMarket</h1>
+      <Button>Click me</Button>
     </>
   )
 }
