d662192c579c37a06e875399bacf2dea5d8d5b7d
Shenon69 <ask.tikki69@gmail.com>
2023-12-30 03:11:58 +0530

    feat: View categories with both levels

--- src/app/(client)/(pages)/order-review/page.tsx
+++ src/app/(client)/(pages)/order-review/page.tsx
@@ -1,8 +1,29 @@
+"use client"
+
 import { BrandIcon, Container, SideMenu } from '@/components/common'
+import { IMainCategory } from '@/types'
+import { getMainAndSubById } from '@/utils/getMainAndSubById'
+import { useEffect, useState } from 'react'
 
 type Props = {}
 
 function OrderReviewPage({ }: Props) {
+
+    const [categories, setCategories] = useState<IMainCategory[]>([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getMainAndSubById(4);
+                setCategories(data);
+            } catch (error) {
+                console.error("Error fetching categories:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <Container>
 
@@ -22,6 +43,37 @@ function OrderReviewPage({ }: Props) {
 
                     <div className='flex items-center justify-center w-full border-2 border-green-400'>
                         <h1>Order Review</h1>
+                        <div>
+                            {categories.map((category) => (
+                                <div className='flex flex-col gap-3' key={category.mainCategoryId}>
+                                    <h2 className='text-green-400'>{category.mainCategoryName}</h2>
+
+                                    {/* Render categoryOnes */}
+                                    <div>
+                                        <h3 className='text-red-200'>Category Ones</h3>
+                                        {category.categoryOnes.map((categoryOne) => (
+                                            <div key={categoryOne.subCatOneId}>
+                                                <p>{categoryOne.subCatOneName}</p>
+                                                {/* Add more details as needed */}
+                                            </div>
+                                        ))}
+                                    </div>
+
+                                    {/* Render categoryTwos */}
+                                    <div>
+                                        <h3 className='text-red-200'>Category Twos</h3>
+                                        {category.categoryOnes.flatMap((categoryOne) =>
+                                            categoryOne.categoryTwos.map((categoryTwo) => (
+                                                <div key={categoryTwo.subCatTwoId}>
+                                                    <p>{categoryTwo.subCatTwoName}</p>
+                                                    {/* Add more details as needed */}
+                                                </div>
+                                            ))
+                                        )}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     </div>
 
                 </div>
